08a43aa03803e279bdb3d3d198238fe225379e51
Florian <p0.florian@gmail.com>
2016-01-28 10:21:42 +0100

    feat(defaultbundle:tag): add function to add tags in an object that supports it
    need to be able to add tags into entities that represent offer and skill

--- src/AGIL/DefaultBundle/Controller/TagController.php
+++ src/AGIL/DefaultBundle/Controller/TagController.php
@@ -2,12 +2,20 @@
 
 namespace AGIL\DefaultBundle\Controller;
 
+use AGIL\OfferBundle\Entity\AgilOffer;
+use AGIL\ProfileBundle\Entity\AgilSkill;
+use InvalidArgumentException;
+use Proxies\__CG__\AGIL\DefaultBundle\Entity\AgilTag;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TagController extends Controller {
 
+	/**
+	 * @param $char Le préfixe
+	 * @return string Ensemble de résultat au format JSON
+	 * Récupère une liste de tags dont le préfixe est $char et la renvoie au format JSON
+	 */
 	public function searchAction($char) {
-
 		$tagsList = $this
 			->getDoctrine()
 			->getManager()
@@ -17,4 +25,32 @@ class TagController extends Controller {
 		return json_encode($tagsList);
 	}
 
+	/**
+	 * @param $object L'objet qui doit (et peut) recevoir un tag
+	 * @param AgilTag $tag Le tag à ajouter
+	 * Ajoute un tag à un objet candidat
+	 */
+	public function addTags($object, Array $tag) {
+
+		// Si c'est une compétence, il n'y a qu'un tag
+		if ($object instanceof AgilSkill) {
+			$object->setTag($tag[0]);
+		}
+		// Si c'est une offre d'emploi/stage
+		elseif ($object instanceof AgilOffer) {
+			/*
+			 * On est obligé d'ajouter les tags un à un
+			 * car la fonction de l'entité ajoute l'argument
+			 * que l'on passe à un tableau.
+			 */
+			foreach($tag as $t) {
+				$object->addTag($t);
+			}
+		}
+		// L'objet ne supporte pas les tags
+		else {
+			throw new InvalidArgumentException('Les tags ne sont pas admis sur cet objet');
+		}
+		$this->addFlash('notice', 'Tags ajoutés avec succès !');
+	}
 }
