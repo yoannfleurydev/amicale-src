25b81e66fa4930bb70c182df2064a48c0df9f55f
Florian POUCHELET <florian.pouchelet@etu.univ-rouen.fr>
2016-01-29 17:14:29 +0100

    refactor(tags) : delete add method
    delete the method to add tag because it was useless

--- src/AGIL/DefaultBundle/Controller/TagController.php
+++ src/AGIL/DefaultBundle/Controller/TagController.php
@@ -43,33 +43,4 @@ class TagController extends Controller {
 		// Retourne le tableau encodé en JSON
 		return new Response(json_encode($jsonTagsList));
 	}
-
-	/**
-	 * @param $object mixed L'objet qui doit (et peut) recevoir un tag
-	 * @param AgilTag $tags Le tag à ajouter
-	 * Ajoute un tag à un objet candidat
-	 */
-	public function addTags($object, Array $tags) {
-
-		// Si c'est une compétence, il n'y a qu'un tag
-		if ($object instanceof AgilSkill ) {
-			$object->setTag($tags[0]);
-		}
-		// Si c'est une offre d'emploi/stage
-		elseif ($object instanceof AgilOffer || $object instanceof AgilForumSubject) {
-			/*
-			 * On est obligé d'ajouter les tags un à un
-			 * car la fonction de l'entité ajoute l'argument
-			 * que l'on passe à un tableau.
-			 */
-			foreach ($tags as $t) {
-				$object->addTag($t);
-			}
-		}
-		// L'objet ne supporte pas les tags
-		else {
-			throw new InvalidArgumentException('Les tags ne sont pas admis sur cet objet');
-		}
-		$this->addFlash('success', 'Tags ajoutés avec succès !');
-	}
 }
