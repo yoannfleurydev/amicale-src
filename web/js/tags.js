ac632da2f3045b81257a2f31724e2197c53b1716
Florian POUCHELET <florian.pouchelet@etu.univ-rouen.fr>
2016-01-29 11:11:22 +0100

    refactor(tags) : install ajax script

--- web/js/tags.js
+++ web/js/tags.js
@@ -1,20 +1,22 @@
 var xhr = new XMLHttpRequest();
+xhr.open('POST', 'search');
 
-xhr.open('POST', 'localhost/app_dev.php/tags/search');
-
+/* On crée un pseudo formulaire pour remplir plus
+   aisément les valeurs dans la requête */
 var data = new FormData();
-form.append('attr1', 'valeur1');
-form.append('attr2', 'valeur2');
+
+// TODO Changer le 2nd argument par la valeur reçue par la saisie utilisateur
+data.append('prefix', 'a');
 
 // pour une requête synchrone il n'y a qu'à vérifier le statut de la requête
-xhr.addEventListener('load', function() {
-    if (xhr.readyState === xhr.DONE) {
-        var response = JSON.parse(xhr.responseText);
-    }
-}, false);
-
-// encodeURIComponent(), afin d'éviter d'écrire d'éventuels caractères interdits dans une URL.
+xhr.addEventListener('load',
+    function() {
+        if (xhr.readyState === xhr.DONE) {
+            // response est un objet JS
+            var response = JSON.parse(xhr.responseText);
+            console.log(response);
+        }
+    },
+    false);
 
 xhr.send(data);
-
-// Traiter les données comme une variable post dans le PHP
