699de127dbf03f757e0d83be11dbc26409168120
Matthieu Coulon <Matthieu Coulon>
2016-02-01 14:48:42 +0100

    ajout admin.user.html modif jsmain.js

--- web/js/jsmain.js
+++ web/js/jsmain.js
@@ -160,15 +160,17 @@ function verifFormAddPost() {
     }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $("#owl-demo").owlCarousel({
 
         autoPlay: 3000, //Set AutoPlay to 3 seconds
 
-        items : 4,
-        itemsDesktop : [1199,3],
-        itemsDesktopSmall : [979,3]
+        items: 4,
+        itemsDesktop: [1199, 3],
+        itemsDesktopSmall: [979, 3]
 
     });
-    });
+});
+
+    $('[data-toggle="tooltip"]').tooltip()
