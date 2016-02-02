566b9821f58a19186af6baf74252e4275eff9176
Mohamed Ibrihen <med.ibrihen@gmail.com>
2016-02-02 05:58:08 +0000

    feat(profile) : edit editAction method of DefaultController

--- src/AGIL/ProfileBundle/Controller/DefaultController.php
+++ src/AGIL/ProfileBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AGIL\ProfileBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -16,8 +17,36 @@ class DefaultController extends Controller
 
     }
 
-    public function editAction()
+    public function editAction($id, Request $request)
     {
+        // EntityManager
+        $em = $this->getDoctrine()->getManager();
+
+        // Récupération de l'objet AgilUser dont l'id est $id
+        $agilUser = $this->$em->getgetRepository('AGILDefaultBundle:AgilUser')->find($id)
+        ;
+
+        if (null === $agilUser) {
+            throw new NotFoundHttpException("L'utilisateur n'existe pas.");
+        }
+
+        // Création du formulaire
+        $form = $this->get('form.factory')->createBuilder('form', $agilUser)
+            ->add('lastName',           'text')
+            ->add('firstName',          'text')
+            ->add('birthdayDate',       'date')
+            ->add('cvUrl',              'url', array('required' => 'false'))
+            ->add('profilePictureUrl',  'url', array('required' => 'false'))
+            ->getForm()
+        ;
+
+        if ($form->handleRequest($request)->isValid()) {
+            $em->flush();
+            $request->getSession()->getFlashBag()->add('notice', 'Profil modifié avec succès.');
+
+            return $this->redirect($this->generateUrl('agil_profile'));
+        }
+
 
     }
 
