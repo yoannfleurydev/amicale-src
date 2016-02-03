80ca13ecae4369e177a011dd3d34fc767497f363
Mohamed Ibrihen <med.ibrihen@gmail.com>
2016-02-03 16:36:58 +0000

    feat(profile) : add testing form to DefaultController

--- src/AGIL/ProfileBundle/Controller/DefaultController.php
+++ src/AGIL/ProfileBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace AGIL\ProfileBundle\Controller;
 
+use AGIL\DefaultBundle\Entity\AgilUser;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,8 +19,9 @@ class DefaultController extends Controller
 
     }
 
-    public function editAction($id, Request $request)
+    public function editAction(Request $request)
     {
+        /*
         // EntityManager
         $em = $this->getDoctrine()->getManager();
 
@@ -28,12 +31,35 @@ class DefaultController extends Controller
 
         if (null === $agilUser) {
             throw new NotFoundHttpException("L'utilisateur n'existe pas.");
-        }
+        }*/
 
+        $agilUser = new AgilUser();
+
+        $agilUser->setFirstName('MyfirstName');
+        $agilUser->setLastName('MylastName');
+        $agilUser->setUsername('MyUserName');
+        $agilUser->setEmail('example@email.com');
+        $agilUser->setCvUrl('mywebsite.com/cv.pdf');
+        $agilUser->setProfilePictureUrl('mywebsite.com/pic.jpg');
+
+        $form = $this->createForm($agilUser)
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('username', TextType::class)
+            ->add('email', TextType::class)
+            ->add('cvUrl', TextType::class)
+            ->add('profilePictureUrl', TextType::class)
+            ->getForm();
+        return $this->render('AGILProfileBundle:Default:index.html.twig', array(
+            'form' => $form->createView()
+        ));
+/*
         // Création du formulaire
         $form = $this->get('form.factory')->createBuilder('form', $agilUser)
-            ->add('lastName',           'text')
             ->add('firstName',          'text')
+            ->add('lastName',           'text')
+            ->add('username',           'text')
+            ->add('email',              'text')
             ->add('birthdayDate',       'date')
             ->add('cvUrl',              'url', array('required' => 'false'))
             ->add('profilePictureUrl',  'url', array('required' => 'false'))
@@ -45,10 +71,8 @@ class DefaultController extends Controller
             $request->getSession()->getFlashBag()->add('notice', 'Profil modifié avec succès.');
 
             return $this->redirect($this->generateUrl('agil_profile'));
-        }
-
+        }*/
 
     }
-
 
 }
