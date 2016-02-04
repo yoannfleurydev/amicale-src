5d8e3463c5ad4f822e11b7565f17b807b60f3b99
Quentin Brodier <quentin.brodier@gmail.com>
2016-02-04 13:36:19 +0100

    fix(forum) : index display

--- web/js/jsmain.js
+++ web/js/jsmain.js
@@ -1,4 +1,4 @@
-$('.collapse').collapse()
+//$('.collapse').collapse()
 
 $('#myCollapsible').collapse({
     toggle: false
@@ -79,8 +79,8 @@ function verifFormulaireAjoutCategorie() {
 $('.icon-add-cat').click(function () {
     getGlyph();
     var nmId = this.id;
-    $('.icon-add-cat').removeClass("active");
-    $('#' + nmId).addClass("active");
+    this.removeClass("active");
+    this.addClass("active");
 });
 
 function getGlyph() {
@@ -159,9 +159,8 @@ function verifFormAddPost() {
         }
     }
 }
-
+/*
 $(document).ready(function () {
-
     $("#owl-demo").owlCarousel({
 
         autoPlay: 3000, //Set AutoPlay to 3 seconds
@@ -171,6 +170,23 @@ $(document).ready(function () {
         itemsDesktopSmall: [979, 3]
 
     });
+});*/
+
+$('[data-toggle="tooltip"]').tooltip();
+
+
+$('.collapse').on('show.bs.collapse', function () {
+    var $id = $(this).attr("id");
+
+    var $fils = $( "a[aria-controls="+$id+"]").children();
+    //$fils.removeClass("animate_down");
+    $fils.addClass("animate_up");
 });
 
-    $('[data-toggle="tooltip"]').tooltip()
+$('.collapse').on('hide.bs.collapse', function () {
+    var $id = $(this).attr("id");
+
+    var $fils = $( "a[aria-controls="+$id+"]").children();
+    $fils.removeClass("animate_up");
+    //$fils.addClass("animate_down");
+});
