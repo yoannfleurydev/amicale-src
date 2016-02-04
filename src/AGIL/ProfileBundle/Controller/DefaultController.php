4e894336e78f944628d001dd2129134deffcfef6
Mohamed Ibrihen <med.ibrihen@gmail.com>
2016-02-04 02:42:14 +0000

    feat(profile) : edit DefaultController, routing.yml and index.html.twig

--- src/AGIL/ProfileBundle/Controller/DefaultController.php
+++ src/AGIL/ProfileBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace AGIL\ProfileBundle\Controller;
 
 use AGIL\DefaultBundle\Entity\AgilUser;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -11,12 +10,21 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AGILProfileBundle:Default:index.html.twig');
+        // Vérifier si l'utilisateur est connecté
+        if (!$this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $user = $this->getUser();
+
+        return $this->render('AGILProfileBundle:Default:index.html.twig', array(
+            'user' => $user,
+        ));
     }
 
     public function showProfileAction($id)
     {
-
+        
     }
 
     public function editAction(Request $request)
@@ -42,7 +50,7 @@ class DefaultController extends Controller
         $agilUser->setCvUrl('mywebsite.com/cv.pdf');
         $agilUser->setProfilePictureUrl('mywebsite.com/pic.jpg');
 
-        $form = $this->createForm($agilUser)
+        $form = $this->createForm("agilUser")
             ->add('firstName', TextType::class)
             ->add('lastName', TextType::class)
             ->add('username', TextType::class)
@@ -50,8 +58,10 @@ class DefaultController extends Controller
             ->add('cvUrl', TextType::class)
             ->add('profilePictureUrl', TextType::class)
             ->getForm();
+
         return $this->render('AGILProfileBundle:Default:index.html.twig', array(
-            'form' => $form->createView()
+            'user' => $agilUser,
+//            'form' => $form->createView()
         ));
 /*
         // Création du formulaire
