aca24855c36e5aac568ff69412ce98f8c4879869
bvince <vi.berniere@gmail.com>
2016-02-04 19:22:26 +0100

    feat(forum) : user can create a new post (subject)

---
+++ src/AGIL/ForumBundle/Form/FirstAnswerType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AGIL\ForumBundle\Form;
+
+use AGIL\ForumBundle\Form\SubjectType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class FirstAnswerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder->add('forumAnswerText', 'textarea', array(
+            'label' => false,
+            'attr' => array(
+                'class' => 'form-control',
+                'placeholder' => 'Première réponse du post',
+            )
+        ));
+
+        $builder->add('subject', new SubjectType(), array ('data_class'   =>  'AGIL\ForumBundle\Entity\AgilForumSubject',));
+
+        $builder->add('Ajouter', 'submit', array(
+            'label' => false,
+            'attr' => array(
+                'class' => 'btn btn-default',
+            )
+        ));
+    }
+
+    public function getName()
+    {
+        return 'forum_add_first_post';
+    }
+}
