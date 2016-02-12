0c0fb613f95529182eb3ad755bba6171b00bb1d1
Florian POUCHELET <florian.pouchelet@etu.univ-rouen.fr>
2016-02-12 11:30:26 +0100

    feat(tags) : complete 3 tests

--- src/AGIL/DefaultBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AGIL/DefaultBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,17 +4,38 @@ namespace AGIL\DefaultBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
-{
-    public function testIndex()
-    {
-        $client = static::createClient();
+class DefaultControllerTest extends WebTestCase {
 
-        $crawler = $client->request('GET', '/');
+	public function testIndex() {
+		$client = static::createClient();
 
-        $this->assertGreaterThan(
-            0,
-            $crawler->filter('html')->count()
-        );
-    }
+		$client->request('GET', '/');
+		$this->assertTrue($client->getResponse()->isSuccessful());
+	}
+
+	public function testTags() {
+		$client = static::createClient();
+
+		/* Si la route renvoie bien une liste contenant albert */
+		$client->request(
+			'POST',
+			'/tags/search',
+			array('prefix' => 'a')
+		);
+
+		$this->assertContains('Acer', $client->getResponse()->getContent());
+
+		/* Si la suppression fonctionne bien */
+		/* Attention, vous devez avoir le tag Web dans la BDD */
+		$client->request(
+			'POST',
+			'/tags/remove',
+			array('tagName' => 'Web')
+		);
+		$this->assertTrue($client->getResponse()->isSuccessful());
+	}
+
+	public function testMailingList() {
+		// TODO Tester les mailing list
+	}
 }
