c5c7500f934ea1e5e8e032d19157fa50f562fa50
Quentin Brodier <quentin.brodier@gmail.com>
2016-02-13 15:04:41 +0100

    feat(forum) : add test example

--- src/AGIL/ForumBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AGIL/ForumBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+    private $client = null;
+
+    /**
+     * "Constructeur" qui initialise le client
+     */
+    public function setUp()
     {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/');
-
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->client = static::createClient();
     }
+
+    /**
+     * Test : Accéder à l'accueil du forum
+     */
+    public function testForumHomepage()
+    {
+        $crawler = $this->client->request('GET', '/forum/');
+        $crawler = $this->client->followRedirect();
+
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username'  => 'superadmin@superadmin.fr',
+            '_password'  => 'superAdmin',
+        ));
+
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertContains('Accueil Forum', $this->client->getResponse()->getContent());
+    }
+
+    /**
+     * Test : Créer un sujet (depuis une catégorie)
+     *
+     * Pré-condition : testForumHomepage()
+     */
+    public function testForumAddSubject()
+    {
+        $this->testForumHomepage();
+
+        // Continuer le test (Déplacement dans une catégorie etc...)
+    }
+
+
 }
