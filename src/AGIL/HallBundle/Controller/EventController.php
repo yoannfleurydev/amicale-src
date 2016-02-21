39d02e8465f864e441169a6d51026ed62954fcb5
Mohamed Ibrihen <sangimed@gmail.com>
2016-02-21 02:35:23 +0100

    feat(Hall) : edit routing.yml and add controllers

---
+++ src/AGIL/HallBundle/Controller/EventController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AGIL\HallBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EventController extends Controller
+{
+    public function showEventAction()
+    {
+    }
+
+    public function showPhotosController()
+    {
+    }
+
+    public function showVideosController()
+    {
+    }
+}
