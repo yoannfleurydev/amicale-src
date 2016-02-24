9f279b1cc3cd5e89df1efb0a2c27d7111ee57b4c
Mohamed Ibrihen <sangimed@gmail.com>
2016-02-24 09:58:13 +0100

    feat(hall) : edit routing, default controller and index view

--- src/AGIL/HallBundle/Controller/DefaultController.php
+++ src/AGIL/HallBundle/Controller/DefaultController.php
@@ -4,10 +4,46 @@ namespace AGIL\HallBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
+class DefaultController extends Controller {
+    /* index */
+    public function indexAction() {
+        $em = $this->getDoctrine()->getManager();
+        $eventRepo = $em->getRepository('AGILHallBundle:AgilEvent');
+        $events = $eventRepo->findAll();
+
+        return $this->render('AGILHallBundle:Default:index.html.twig', array('listEvents' => $events));
+    }
+
+    /* Affichage de la page souhaitée */
+    public function renderPageAction($idPage) {
+        // TODO Faire la pagination
         return $this->render('AGILHallBundle:Default:index.html.twig');
+    }
+
+    /* Affichage d'un événement */
+    public function eventAction($idEvent) {
+        $em = $this->getDoctrine()->getManager();
+        $eventRepo = $em->getRepository('AGILHallBundle:AgilEvent');
+        $event = $eventRepo->find($idEvent);
+
+        return $this->render('AGILHallBundle:Default:event.html.twig', array('event' => $event));
+    }
+
+    /* Affichage des photos d'un événement */
+    public function photosEventAction($idEvent) {
+        $em = $this->getDoctrine()->getManager();
+        $photoRepo = $em->getRepository('AGILHallBundle:AgilPhoto');
+        $photos = $photoRepo->findByEvent($idEvent);
+
+        return $this->render('AGILHallBundle:Default:photoEvent.html.twig', array('photos' => $photos));
+    }
+
+    /* Affichage des vidéos d'un événement */
+    public function videosEventAction($idEvent) {
+        $em = $this->getDoctrine()->getManager();
+        $videoRepo = $em->getRepository('AGILHallBundle:AgilPhoto');
+        $videos = $videoRepo->findByEvent($idEvent);
+
+        return $this->render('AGILHallBundle:Default:videoEvent.html.twig');
     }
 }
