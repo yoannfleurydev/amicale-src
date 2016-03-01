bd2d5bef43d618cb08aa7953588d37cd3dd2e0d4
valentinCro <valentin.crochemore1@etu.univ-rouen.fr>
2016-03-01 11:39:10 +0100

    feat : calendar front breadcrumbs front

--- web/js/fullcalendar.js
+++ web/js/fullcalendar.js
@@ -9688,7 +9688,7 @@ function Header(calendar, options) {
 					var button; // the element
 
 					if (buttonName == 'title') {
-						groupChildren = groupChildren.add($('<h2>&nbsp;</h2>')); // we always want it to take up height
+						groupChildren = groupChildren.add($('<h4>&nbsp;</h4>')); // we always want it to take up height
 						isOnlyButtons = false;
 					}
 					else {
@@ -9828,7 +9828,7 @@ function Header(calendar, options) {
 	
 	
 	function updateTitle(text) {
-		el.find('h2').text(text);
+		el.find('h4').text(text);
 	}
 	
 	
