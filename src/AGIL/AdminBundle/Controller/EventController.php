55b656208f9356d2b795147ae484e384208642aa
sangimed <sangimed@gmail.com>
2016-03-02 01:30:38 +0100

    feat(hall) : move & edit admin related actions from hall bundle to admin bundle

--- src/AGIL/AdminBundle/Controller/EventController.php
+++ src/AGIL/AdminBundle/Controller/EventController.php
@@ -2,13 +2,74 @@
 
 namespace AGIL\AdminBundle\Controller;
 
+use AGIL\HallBundle\Entity\AgilEvent;
+use AGIL\HallBundle\Form\EventType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class EventController extends Controller
 {
-    public function adminEventAddAction()
-    {
+    public function adminEventAction() {
 
-        return $this->render('AGILAdminBundle:Event:admin.event.add.html.twig');
+    }
+
+    public function adminEventAddAction(Request $request) {
+        $event = new AgilEvent();
+
+        $form = $this->createForm(new EventType(), $event);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $event->setUser($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+
+            $this->addFlash('success', 'Evenement ajouté');
+
+            return $this->redirect($this->generateUrl('agil_admin_events', array('id' => $event->getEventId())));
+        }
+        return $this->render('AGILAdminBundle:Event:admin_event_add.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function adminEventEditAction ($idEvent, Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $event = $em->getRepository('AGILHallBundle:AgilEvent')->find($idEvent);
+
+        if (null === $event) {
+            throw new NotFoundHttpException("L'evénement d'id " . $id . " n'existe pas.");
+        }
+
+        $form = $this->createForm(new EventType(), $event);
+
+        // Remplissage des champs qui ont une valeur
+        $form->get('eventTitle')->setData($event->getEventTitle());
+        $form->get('eventText')->setData($event->getEventText());
+        $form->get('eventDate')->setData($event->getEventDate());
+
+        $form->handleRequest($request);
+
+        if($form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', "L'évenement a été modifié");
+
+            return $this->redirect($this->generateUrl('agil_admin_events', array('id' => $event->getEventId())));
+        }
+
+        return $this->render('AGILAdminBundle:Event:admin_event_edit.html.twig', array(
+            'event' => $event,
+            'form'  => $form->createView()
+        ));
+
+        return $this->render('AGILAdminBundle:Event:admin_event_edit.html.twig');
+    }
+
+    public function adminEventDeleteAction ($idEvent) {
+
+        return $this->redirect($this->generateUrl('AGILHallBundle:Event:admin_event.html.twig'));
     }
 }
