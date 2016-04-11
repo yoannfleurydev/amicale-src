d2e5560490e997fbb5fc64b0a1736e1ca75514dc
Yoann Fleury <yoann.fleury@yahoo.com>
2016-04-11 17:04:20 +0200

    feat(offer) : add offer to home page

--- src/AGIL/DefaultBundle/Controller/DefaultController.php
+++ src/AGIL/DefaultBundle/Controller/DefaultController.php
@@ -18,12 +18,19 @@ class DefaultController extends Controller
 
         // Repository des Sujets de forum
         $subjectRepository = $manager->getRepository('AGILForumBundle:AgilForumSubject');
+        $offerRepository = $manager->getRepository('AGILOfferBundle:AgilOffer');
 
         // Récupération des 3 derniers sujets de forum créés
         $lastSubjects = $subjectRepository->findBy(array(),array('forumSubjectPostDate' => 'desc'),3);
+        // Récupération des 3 dernières annonces créés
+        $lastOffers = $offerRepository->findBy(array('offerPublish' => true), array('offerPostDate' => 'desc'), 3);
 
         return $this->render('AGILDefaultBundle:Default:index.html.twig',
-            array('lastSubjects' => $lastSubjects));
+            array(
+                'lastSubjects' => $lastSubjects,
+                'offers' => $lastOffers
+            )
+        );
     }
 
     public function creditsAction() {
