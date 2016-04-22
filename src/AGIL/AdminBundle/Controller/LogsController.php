eff529a162b2385b9ace30244f96dde7d4d35d9e
Quentin Brodier <quentin.brodier@gmail.com>
2016-04-22 14:03:22 +0200

    feat(logs) : add files for logs Profile, Offer, Hall & Chat

--- src/AGIL/AdminBundle/Controller/LogsController.php
+++ src/AGIL/AdminBundle/Controller/LogsController.php
@@ -17,7 +17,7 @@ class LogsController extends Controller
      */
     public function forumAction(Request $request)
     {
-        $pathLogFile = $this->get('kernel')->getRootDir().'/logs/logsForum.log';
+        $pathLogFile = $this->get('kernel')->getLogDir().'/logsForum.log';
 
         // Suppression du fichier
         if($request->isMethod('POST')){
@@ -47,7 +47,7 @@ class LogsController extends Controller
      */
     public function searchAction(Request $request)
     {
-        $pathLogFile = $this->get('kernel')->getRootDir().'/logs/logsSearch.log';
+        $pathLogFile = $this->get('kernel')->getLogDir().'/logsSearch.log';
 
         // Suppression du fichier
         if($request->isMethod('POST')){
@@ -66,6 +66,126 @@ class LogsController extends Controller
         }
 
         return $this->render('AGILAdminBundle:Logs:logs_search.html.twig',array('logsSearch' => $contentLogFile));
+    }
+
+
+    /**
+     * Affichage des logs des Offres
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function offerAction(Request $request)
+    {
+        $pathLogFile = $this->get('kernel')->getLogDir().'/logsOffer.log';
+
+        // Suppression du fichier
+        if($request->isMethod('POST')){
+            if (file_exists($pathLogFile)) {
+                unlink($pathLogFile);
+                $this->addFlash('success', "Le fichier de log est maintenant réinitialisé");
+            }else{
+                $this->addFlash('warning', "Le fichier de log est déjà réinitialisé");
+            }
+        }
+
+        if (file_exists($pathLogFile)) {
+            $contentLogFile = file_get_contents($pathLogFile, true);
+        }else{
+            $contentLogFile = "Fichier vide";
+        }
+
+        return $this->render('AGILAdminBundle:Logs:logs_offer.html.twig',array('logsOffer' => $contentLogFile));
+    }
+
+
+    /**
+     * Affichage des logs des Profils
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function profileAction(Request $request)
+    {
+        $pathLogFile = $this->get('kernel')->getLogDir().'/logsProfile.log';
+
+        // Suppression du fichier
+        if($request->isMethod('POST')){
+            if (file_exists($pathLogFile)) {
+                unlink($pathLogFile);
+                $this->addFlash('success', "Le fichier de log est maintenant réinitialisé");
+            }else{
+                $this->addFlash('warning', "Le fichier de log est déjà réinitialisé");
+            }
+        }
+
+        if (file_exists($pathLogFile)) {
+            $contentLogFile = file_get_contents($pathLogFile, true);
+        }else{
+            $contentLogFile = "Fichier vide";
+        }
+
+        return $this->render('AGILAdminBundle:Logs:logs_profile.html.twig',array('logsProfile' => $contentLogFile));
+    }
+
+
+    /**
+     * Affichage des logs du Chat
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function chatAction(Request $request)
+    {
+        $pathLogFile = $this->get('kernel')->getLogDir().'/logsChat.log';
+
+        // Suppression du fichier
+        if($request->isMethod('POST')){
+            if (file_exists($pathLogFile)) {
+                unlink($pathLogFile);
+                $this->addFlash('success', "Le fichier de log est maintenant réinitialisé");
+            }else{
+                $this->addFlash('warning', "Le fichier de log est déjà réinitialisé");
+            }
+        }
+
+        if (file_exists($pathLogFile)) {
+            $contentLogFile = file_get_contents($pathLogFile, true);
+        }else{
+            $contentLogFile = "Fichier vide";
+        }
+
+        return $this->render('AGILAdminBundle:Logs:logs_chat.html.twig',array('logsChat' => $contentLogFile));
+    }
+
+
+    /**
+     * Affichage des logs du Hall
+     *
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function hallAction(Request $request)
+    {
+        $pathLogFile = $this->get('kernel')->getLogDir().'/logsHall.log';
+
+        // Suppression du fichier
+        if($request->isMethod('POST')){
+            if (file_exists($pathLogFile)) {
+                unlink($pathLogFile);
+                $this->addFlash('success', "Le fichier de log est maintenant réinitialisé");
+            }else{
+                $this->addFlash('warning', "Le fichier de log est déjà réinitialisé");
+            }
+        }
+
+        if (file_exists($pathLogFile)) {
+            $contentLogFile = file_get_contents($pathLogFile, true);
+        }else{
+            $contentLogFile = "Fichier vide";
+        }
+
+        return $this->render('AGILAdminBundle:Logs:logs_hall.html.twig',array('logsHall' => $contentLogFile));
     }
 
 
